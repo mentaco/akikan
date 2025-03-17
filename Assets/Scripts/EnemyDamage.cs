b4c9d19e628f6b5e42b6457b5bcb4795275a794b
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2025-03-17 19:23:00 +0900

    Destruction of enemy's parts

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -8,6 +8,9 @@ public class EnemyDamage : MonoBehaviour
     int limit_dm = 3;
 
     [SerializeField]
+    float blown_force = 700;
+
+    [SerializeField]
     private ParticleSystem particle;
 
     // Start is called before the first frame update
@@ -25,12 +28,19 @@ public class EnemyDamage : MonoBehaviour
     void OnCollisionEnter(Collision c)
     {
         limit_dm--;
-        Debug.Log(limit_dm);
         if (limit_dm < 1)
         {
             if (c.gameObject.CompareTag("Bullet"))
             {
-                Instantiate(particle, this.transform.position, Quaternion.identity);
+                // 吹き飛ばし処理
+                Vector3 direction = c.relativeVelocity.normalized * blown_force;
+                Rigidbody p_rigidbody = transform.parent.GetComponent<Rigidbody>();
+                p_rigidbody.AddForce(direction.x, blown_force / 2, direction.z, ForceMode.Impulse);
+
+                // 爆発のエフェクト
+                Instantiate(particle, c.transform.position, Quaternion.identity);
+
+                // 非表示処理
                 this.gameObject.SetActive(false);
             }
         }
